5e4e27ab508642b1f734ce6a17398a24ffd886ec
Evgeny Kravchenko <evgen.kravchenko.vladimirovich@mail.ru>
2020-07-06 23:42:41 +0300

    refactor: add mock data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
 
+import mockData from '../mock/mock-data';
+
 import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      msrp: '',
+      vehicleName: '',
+      dealerName: '',
+      dealerPhone: '',
+      dealerRating: '',
+    };
+  }
+
+  componentDidMount() {
+    mockData.then((resolve) => {
+      setTimeout(() => {
+        this.setState({
+          msrp: resolve.msrp,
+          vehicleName: resolve.vehicleName,
+          dealerName: resolve.dealerName,
+          dealerPhone: resolve.dealerPhone,
+          dealerRating: resolve.dealerRating,
+        });
+      }, 1000);
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>My React App!</h1>
+        <h1>React app</h1>
       </div>
     );
   }
 }
-
 
 export default App;
